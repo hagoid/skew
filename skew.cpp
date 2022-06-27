fd9cd9455d2e303c23f07131a0858174434124be
Michal Hagara <michal.hagara@gmail.com>
2022-06-27 13:28:51 +0200

    140421

--- skew.cpp
+++ skew.cpp
@@ -4,264 +4,205 @@
 
 using namespace std;
 
-int rad(int n, int t){
-	int a, i;
-	for(i=1,a=1;(a = (a*t)%n)!=1;i++);
-	return i;
-}
-
-int gcd(int n, int k){
-	if(n%k == 0) return k;
-	return gcd(k, n%k);
-}
-
-int lcm(int n, int k){
-	return n*k / gcd(n,k);
-}
+class orbit{
+	public:
+		int * orbita;
+		int * pi;
+		int dlzka;
+		orbit(int tdlzka);
+		~orbit();
+};
 
 class permutation{
 	public:
-		int n, cn;
-		int *predpis;
-		int *pi;
-		int *cyklus;
-		bool *nastavene;
-		int rad();
+		int n;
+		orbit ** orbity;
+		int * poradie;
+		int nasledovnik(int a, int k);
 		void reset();
+		void nacitaj();
 		void vypis();
+		void vypis_pi();
+		void pi_na_orbite();
+		void dopocitaj_z_orbity();
+		void z_predpisu(int * predpis, int * pi);
 		permutation(int tn);
+		~permutation();
 };
+
+orbit::orbit(int tdlzka){
+	dlzka = tdlzka;
+	orbita = new int[dlzka];
+	pi = new int[dlzka];
+	for(int i=0; i<dlzka; i++){
+		pi[i] = 0;
+	}
+}
+
+orbit::~orbit(){
+	delete orbita;
+	delete pi;
+}
 
 permutation::permutation(int tn){
 	n = tn;
-	predpis = new int[n];
-	nastavene = new bool[n];
-	pi = new int[n];
-	cyklus = NULL;
+	orbity = new orbit *[n];
+	poradie = new int[n];
+	for(int i=0; i<n; i++){
+		orbity[i] = NULL;
+		poradie[i] = 0;
+	}
+}
+
+permutation::~permutation(){
 	reset();
+	delete poradie;
+	delete orbity;
 }
 
 void permutation::reset(){
-	for(int i=0; i<n; i++){
-		predpis[i] = i;
-		nastavene[i] = false;
-		pi[i] = 1;
+	bool znicene[n];
+	int i;
+	for(i=0; i<n; i++){
+		if(poradie[i] == 0) delete orbity[i];
+		orbity[i] = NULL;
+		poradie[i] = 0;
 	}
-	delete cyklus;
-	cyklus = NULL;
 }
 
-int permutation::rad(){
-	int r = 1;
-	int d, m;
-	bool uz[n];
-	for(int i=0; i<n; i++) uz[i] = false;
-	for(int i=0; i<n; i++){
-		if(!uz[i]){
-			uz[i] = true;
-			for(d=1,m=i;(m = predpis[m]) != i;d++) uz[m] = true;
-			r = lcm(r, d);
+void permutation::z_predpisu(int * predpis, int * pi){
+	int dlzka;
+	for(i=0; i<n; i++){
+		if(orbity[i] == NULL){
+			dlzka = 0;
+			for() OTESTUJ TO
 		}
 	}
-	return r;
+}
+
+int permutation::nasledovnik(int a, int k = 1){
+	if(orbity[a] == NULL) return a;
+	return orbity[a]->orbita[(poradie[a]+k)%orbity[a]->dlzka];
+}
+
+void permutation::nacitaj(){
+	int p, d;
+	orbit * o;
+	cin >> p;
+	for(int i=0; i<p; i++){
+		cin >> d;
+		o = new orbit(d);
+		for(int j=0; j<d; j++){
+			cin >> o->orbita[j];
+			orbity[ o->orbita[j] ] = o;
+			poradie[ o->orbita[j] ] = j;
+		}
+	}
 }
 
 void permutation::vypis(){
-	int m;
-	bool uz[n], jeden;
-	for(int i=0; i<n; i++) uz[i] = false;
+	bool nevypisane[n];
 	for(int i=0; i<n; i++){
-		if(!uz[i]){
-			m = i;
-			jeden = true;
-			while(predpis[m]!=i){
-				if(jeden){
-					cout<<"("<<i;
-					uz[i] = true;
-					jeden = false;
-				}
-				cout<<", ";
-				m = predpis[m];
-				uz[m] = true;
-				cout << m;
+		if(orbity[i] && !poradie[i]){
+			cout << "(" << orbity[i]->orbita[0];
+			for(int j=1; j<orbity[i]->dlzka; j++){
+				cout << ", " << orbity[i]->orbita[j];
 			}
-			if(!jeden) cout << ")";
+			cout << ")";
 		}
 	}
-	cout << "\n";
 }
 
-int permutuj(int k, int t, int h, permutation * phi){
-	phi->reset();
-	int i, p, rH;
-	for(i=0;i<phi->n;i+=k){
-		phi->predpis[i] = (phi->predpis[i]*t) % phi->n;
-		phi->nastavene[i] = true;
-	}
-	rH = phi->rad();
-	for(i=1, p=1; (phi->predpis[i] = (i + h)%phi->n) != 1; h=(h*t)%phi->n, i = phi->predpis[i], p++) phi->nastavene[i] = true;
-	phi->nastavene[i] = true;
-	phi->cn = p;
-	phi->cyklus = new int[p];
-	phi->cyklus[0] = 1;
-	for(i=1;i<p;i++) phi->cyklus[i] = phi->predpis[phi->cyklus[i-1]];
-	return rH;
-}
-
-bool over(int k, int t, int pi, int rH, permutation * phi){
-	int mocnina = 1;
-	int vysledok = phi->predpis[1];
-	for(int i=1; i<k; i++){
-		mocnina = (mocnina*pi)%phi->cn;
-		vysledok = (vysledok + phi->cyklus[mocnina])%phi->n;
-	}
-	return ((k*t)%phi->n == vysledok) && ((pi-1)%rH == 0);
-}
-
-void dopermutuj(int pi, permutation * phi){
-	int vysledok;
-	int mocnina;
-	for(int a=1; a<phi->n; a++) if(!phi->nastavene[a]){
-		mocnina = 1;
-		vysledok = phi->predpis[1];
-		for(int i=1; i<a; i++){
-			mocnina = (mocnina*pi)%phi->cn;
-			vysledok = (vysledok + phi->cyklus[mocnina])%phi->n;
+void permutation::vypis_pi(){
+	bool nevypisane[n];
+	for(int i=0; i<n; i++){
+		if(orbity[i] && !poradie[i]){
+			cout << "[" << orbity[i]->pi[0];
+			for(int j=1; j<orbity[i]->dlzka; j++){
+				cout << ", " << orbity[i]->pi[j];
+			}
+			cout << "]";
 		}
-		phi->predpis[a] = vysledok;
-		phi->nastavene[a] = true;
 	}
-	phi->pi[1] = pi;
 }
 
-int delitelov(int n){
-	int pocet = 0;
-	for(int i=1;i<=n;i++) if(n%i==0) pocet++;
-	return pocet;
-}
-
-bool porovnaj(permutation *a, permutation *b){
-	bool ok = true;
-	if(a->n != b->n) return false;
-	for(int i = 0;i<a->n;i++){
-		ok &= (a->predpis[i] == b->predpis[i]);
+void permutation::pi_na_orbite(){
+	if(orbity[1] == NULL || orbity[n-nasledovnik(1)] == NULL){
+		printf("Neni orbita jednotky.\n");
+		return;
 	}
-	ok &= (a->pi[1] == b->pi[1]);
-	return ok;
+	int j;
+	for(int i=0; i < orbity[1]->dlzka; i++){
+		if(orbity[n-nasledovnik(1)] != orbity[n-1]){
+			printf("Neni su orbity spravne.\n");
+			return ;
+		}
+		j = orbity[1]->orbita[i];
+		orbity[1]->pi[i] = (poradie[n-nasledovnik(j)] - poradie[n-j] + orbity[n-j]->dlzka) % orbity[n-j]->dlzka;
+	}
 }
 
-int najdi(int n, int pocet = 0, permutation **zoznam = NULL){
-	int k, t, h, r, rH, pi, vysledok=0;
-	bool check[pocet], checked;
-	for(int i=0;i<pocet;i++) check[i]=false;
-	permutation *phi = new permutation(n);
-	//cout << "Nasli sme:\n";
-	for(k = 2; k<=n; k++){
-		if(n%k == 0){
-			for(t = 1; t <= n/k; t++){
-				if(gcd(n/k, t) == 1){
-					for(h = 0; h < n; h += k){
-						rH = permutuj(k,t,h,phi);
-						r = phi->rad();
-						for(pi = 1; pi<r; pi++){
-							if(gcd(r, pi) == 1){
-								if(rad(r, pi) == k){
-									if(over(k,t,pi,rH,phi)){
-										dopermutuj(pi,phi);
-										if(pocet){
-											checked = false;
-											for(int i=0;i<pocet;i++){
-												if(porovnaj(zoznam[i],phi)){
-													check[i] = true;
-													checked = true;
-												}
-											}
-											if(!checked){
-												phi->vypis();
-												cout <<n<<" "<<k<<" "<<t<<" "<<h<<" "<<pi<<"\n";
-											}
-										}
-										else if(phi->rad()!=n/k){
-											
-											//phi->vypis();
-											//cout <<n<<" "<<k<<" "<<t<<" "<<h<<" "<<pi<<"\n";
-										}
-										vysledok++;
-										phi->reset();
-										permutuj(k,t,h,phi);
-									}
-								}
-							}
-						}
-					}
-				}
+void permutation::dopocitaj_z_orbity(){
+	if(orbity[1] == NULL){
+		//printf("Neni orbita jednotky.\n");
+		return;
+	}
+	int d = orbity[1]->dlzka;
+	int pi[n], predpis[n], sucet[d+1], i;
+	sucet[0] = 0;
+	for(i=0; i<d; i++){
+		sucet[i+1] = sucet[i] + orbity[1]->pi[(poradie[1]+i)%d];
+	}
+	pi[1] = sucet[1];
+	for(i=2; i<n; i++){
+		pi[i] = (sucet[pi[i-1]%d] + sucet[d]*(pi[i-1]/d)) % d;
+	}
+	pi[0] = (sucet[pi[n-1]%d] + sucet[d]*(pi[n-1]/d)) % d;
+	if(pi[0] != 1){
+		//printf("Nesedi nam pi(0).\n");
+		return;
+	}
+	for(i=1; i<d; i++){
+		if(pi[orbity[1]->orbita[i]] != orbity[1]->pi[i]){
+			//printf("Nesedia nam pi.\n");
+			return;
+		}
+	}
+	
+	predpis[1] = nasledovnik(1);
+	for(i=2; i<n; i++){
+		predpis[i] = (predpis[i-1] + nasledovnik(1, pi[i-1])) % n;
+	}
+	predpis[0] = (predpis[n-1] + nasledovnik(1, pi[n-1])) % n;
+	if(predpis[0] != 0){
+		//printf("Nesedi nam predpis 0.\n");
+		return;
+	}
+	for(i=0; i<n; i++){
+		if(orbity[i] != NULL){
+			if(predpis[i] != nasledovnik(i)){
+				//printf("Nesedia nam predpisy.\n");
+				return;
 			}
 		}
 	}
-	//cout << "Nenasli sme:\n";
-	for(int i=0;i<pocet;i++) if(!check[i]) zoznam[i]->vypis();
-	return vysledok;
-}
-
-bool coel(permutation *phi){
-	bool ok = true;
-	ok &= (phi->pi[1]!=1);
-	for(int i=1; i<phi->cn; i++) ok &= (phi->pi[1] == (phi->pi[phi->cyklus[i]]));
-	return ok;
+	
+	reset();
+	z_predpisu(predpis, pi);
+	
+	//printf("Vsetko ok.\n");
 }
 
 int main(){
-	ifstream f;
-	f.open("zoznam/zoznam");
-	int n, pocet, cyklov, dlzka, naozaj, a, b , z;
-	permutation *zoznam[100];
-	permutation *phi;
-	/*for(int i = 2; i<=52; i++){
-		naozaj = 0;
-		f >> n >> pocet;
-		cout << n << "\n";
-		phi = new permutation(n);
-		for(int j=0; j<pocet; j++){
-			phi->reset();
-			f >> cyklov;
-			if(cyklov == 0){
-				phi->cn = 1;
-				phi->cyklus = new int[1];
-				phi->cyklus[0] = 1;
-			}
-			for(int k=0; k<cyklov; k++){
-				f >> dlzka;
-				f >> z;
-				if(k == 0){
-					phi->cn = dlzka;
-					phi->cyklus = new int[dlzka];
-					phi->cyklus[0] = 1;
-				}
-				a = z;
-				for(int l=1; l<dlzka; l++){
-					f >> b;
-					if(k == 0) phi->cyklus[l] = b;
-					phi->predpis[a] = b;
-					a = b;
-				}
-				phi->predpis[a] = z;
-			}
-			for(int k=0; k<n; k++){
-				f >> phi->pi[k];
-			}
-			if(coel(phi)){
-				zoznam[naozaj++] = phi;
-				phi = new permutation(n);
-			}
-		}
-		
-		najdi(n, naozaj, zoznam);
-		for(int j=0; j<naozaj; j++) delete zoznam[j];
-	}*/
-	f.close();
-	
-	for(n=2;n<=300;n++){
-		pocet = najdi(n);
-		if(pocet) cout << "Pre n="<<n<<" je pocet " << pocet<<"\n";
+	int n, pocet;
+	cin >> n >> pocet;
+	permutation * phi = new permutation(n);
+	for(int i=0; i<pocet; i++){
+		phi->reset();
+		phi->nacitaj();
+		phi->pi_na_orbite();
+		phi->dopocitaj_z_orbity();
 	}
+	return 0;
 }
+
